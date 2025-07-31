ee86890cdbe88dfe2a0c9c0d21e211953ec03f0c
Sanatptl <sanatpatel96@gmail.com>
2025-08-01 02:17:22 +0530

    fix: minor bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,12 +7,11 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 import path from "path";
-import { fileURLToPath } from "url";
+// import { fileURLToPath } from "url";
 
 dotenv.config({ path: "./.env" });
 connectDB();
 const PORT = process.env.PORT || 5000;
-// const app = express();
 
 const corsOptions = { origin: "http://localhost:5173", credentials: true };
 app.use(cors(corsOptions));
@@ -24,12 +23,12 @@ app.use(express.json({ limit: "20mb" }));
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/", (req, res) => {
+  app.get("/*splat", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
